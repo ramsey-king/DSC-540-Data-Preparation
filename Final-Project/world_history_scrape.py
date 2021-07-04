860190f6b84fa3c238df4fce82000060d5e8ed55
ramsey-king <75282693+ramsey-king@users.noreply.github.com>
2021-07-03 22:03:26 -0500

    World History Project Python file to scrape headline events and put into a table for sentiment analysis.

--- Final-Project/world_history_scrape.py
+++ Final-Project/world_history_scrape.py
@@ -1,20 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import sys
+import pandas as pd
+import numpy as np
 
-year = 2012
+year = 1942
 page_num = 1
 url = 'https://worldhistoryproject.org/' + str(year) + '/page/' + str(page_num)
 webpage_contents = requests.get(url).text
 response = requests.get(url)
 soup = BeautifulSoup(webpage_contents, "lxml")
 
-headings = []
+dates, title, headlines = [], [], []
 
 while True:
     print(url)
     if year == 2013:
-        print(dates, headlines)
+        # print(len(dates), len(title), len(headlines))
+        data = {'Date': dates, 'Title': title, 'Headline': headlines}
+        world_history_df = pd.DataFrame(data)
+        # print(world_history_df.head())
+        world_history_df.to_csv('world_history_project.csv')
         sys.exit()
     webpage_contents = requests.get(url).text
     soup = BeautifulSoup(webpage_contents, "lxml")
@@ -22,16 +28,24 @@ while True:
     url = 'https://worldhistoryproject.org/' + str(year) + '/page/' + str(page_num)
     result = soup.find('div', {'class': 'col-xs-12 pagination-container'}).find('ul').find('li', {'class': 'next disabled'})
 
-    dates = []
-    for li_tag in soup.find_all('li', {'class': 'media-event'}):
+    # dates = []
+    for li_tag in soup.find_all('li', {'class': 'media event'}):
         time_info = li_tag.find('time')
-        print(time_info)
+        # dates.append(time_info.attrs['datetime'])
         dates.append(time_info.text.strip())
 
-    headlines = []
+    # title = []
+    for h3_tag in soup.find_all('h3', {'class': 'media-heading'}):
+        title_info = h3_tag.find('a')
+        title.append((title_info.text.strip()))
+
+    # headlines = []
     for div_tag in soup.find_all('div', {'class': 'media-body'}):
-        p_tag = div_tag.find('p')
-        headlines.append(p_tag.text.strip())
+        try:
+            p_tag = div_tag.find('p')
+            headlines.append(p_tag.text.strip())
+        except AttributeError:
+            headlines.append('NO HEADLINE DATA ENTERED')
 
     if result is not None:
         year += 1
