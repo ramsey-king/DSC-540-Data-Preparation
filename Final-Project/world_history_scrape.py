2b502e6095a0001c0e89db9b0a6b83ea8b7b78a5
ramsey-king <raking@my365.bellevue.edu>
2021-07-08 06:32:54 -0500

    08 July 21 0632

--- Final-Project/world_history_scrape.py
+++ Final-Project/world_history_scrape.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 import numpy as np
 
-year = 1942
+year = 2012
 page_num = 1
 url = 'https://worldhistoryproject.org/' + str(year) + '/page/' + str(page_num)
 webpage_contents = requests.get(url).text
@@ -13,39 +13,49 @@ soup = BeautifulSoup(webpage_contents, "lxml")
 
 dates, title, headlines = [], [], []
 
-while True:
-    print(url)
-    if year == 2013:
-        # print(len(dates), len(title), len(headlines))
-        data = {'Date': dates, 'Title': title, 'Headline': headlines}
-        world_history_df = pd.DataFrame(data)
-        # print(world_history_df.head())
-        world_history_df.to_csv('world_history_project.csv')
-        sys.exit()
-    webpage_contents = requests.get(url).text
-    soup = BeautifulSoup(webpage_contents, "lxml")
-    page_num += 1
-    url = 'https://worldhistoryproject.org/' + str(year) + '/page/' + str(page_num)
-    result = soup.find('div', {'class': 'col-xs-12 pagination-container'}).find('ul').find('li', {'class': 'next disabled'})
 
-    # dates = []
+def get_dates():
     for li_tag in soup.find_all('li', {'class': 'media event'}):
         time_info = li_tag.find('time')
         # dates.append(time_info.attrs['datetime'])
         dates.append(time_info.text.strip())
 
-    # title = []
+
+def get_titles():
     for h3_tag in soup.find_all('h3', {'class': 'media-heading'}):
         title_info = h3_tag.find('a')
         title.append((title_info.text.strip()))
 
-    # headlines = []
+
+def get_info():
     for div_tag in soup.find_all('div', {'class': 'media-body'}):
         try:
             p_tag = div_tag.find('p')
             headlines.append(p_tag.text.strip())
         except AttributeError:
             headlines.append('NO HEADLINE DATA ENTERED')
+
+
+while True:
+    print(url)  # here for now to make sure things work.
+    if year == 2013:
+        # print(len(dates), len(title), len(headlines))
+        data = {'Date': dates, 'Title': title, 'Headline': headlines}
+        world_history_df = pd.DataFrame(data)
+        # print(world_history_df.head())
+        # world_history_df.to_csv('world_history_project.csv') PUT BACK IN WHEN READY TO TURN IN PROJECT
+        sys.exit()
+
+    webpage_contents = requests.get(url).text
+    soup = BeautifulSoup(webpage_contents, "lxml")
+    page_num += 1
+    url = 'https://worldhistoryproject.org/' + str(year) + '/page/' + str(page_num)
+    result = soup.find('div', {'class': 'col-xs-12 pagination-container'}).find('ul').find('li',
+                                                                                           {'class': 'next disabled'})
+
+    get_dates()
+    get_titles()
+    get_info()
 
     if result is not None:
         year += 1
@@ -55,4 +65,9 @@ while True:
     else:
         continue
 
-
+'''
+Transformations needed to make:  
+1.  Format all the dates into one type.  If a date has date to date, remove the second date.
+2.  Strip whitespace from Title and Headline columns
+3.  Replace or create headers was done in the Python code. 
+'''
