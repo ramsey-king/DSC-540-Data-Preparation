808fc25c7e476538ab4c6d0b6b87607c0e4c8952
ramsey-king <75282693+ramsey-king@users.noreply.github.com>
2021-07-15 06:13:53 -0500

    14 July 1300

--- Final-Project/citation_index_scrape_selenium.py
+++ Final-Project/citation_index_scrape_selenium.py
@@ -29,31 +29,59 @@ talk_num = 2
 url = 'https://scriptures.byu.edu/#:t' + str(talk_num)
 
 
-path_linux = "/home/ramsey/Documents/chromedriver"
-driver = webdriver.Chrome(path_linux)
+# path_linux = "/home/ramsey/Documents/chromedriver"
+# driver = webdriver.Chrome(path_linux)
+
+path_pc = 'C:\\Users\\Ramsey\\Downloads\\chromedriver.exe'
+driver = webdriver.Chrome(path_pc)
 driver.get(url)
 # print(driver.title)
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
 
 # search = driver.find_element_by_id('centercolumn')
+talk_title, speaker, position, bibliography, talk_contents = [], [], [], [], []
 
-try:
-    webpage = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "centercolumn"))
-    )
+def get_talk():
+    try:
+        webpage = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "centercolumn"))
+        )
 
-    # print(webpage.text)
-    talk_header_info = webpage.find_elements_by_class_name('gchead')
-    for element in talk_header_info:
-        print(element.text)
+        # print(webpage.text)
+        talk_header_info = webpage.find_elements_by_class_name('gchead')
+        for element in talk_header_info:
+            talk_title.append(webpage.find_element_by_class_name('gctitle').text)
+            speaker.append(webpage.find_element_by_class_name('gcspeaker').text)
+            position.append(webpage.find_element_by_class_name('gcspkpos').text)
+            bibliography.append(webpage.find_element_by_class_name('gcbib').text)    
 
-    print(webpage.find_element_by_class_name('gctitle').text)
+        paragraphs = webpage.find_elements_by_class_name("gcbody")
+        for paragraph in paragraphs:
+            talk_contents.append(paragraph.text)
 
-    paragraphs = webpage.find_elements_by_class_name("gcbody")
-    for paragraph in paragraphs:
-        print(paragraph.text)
-finally:
-    driver.quit()
+        print(talk_title, len(talk_contents))
+        
+    finally:
+        driver.quit()
 
+def make_dataset():
+    data = {'Title': talk_title, 'Speaker': speaker, 'Position': position, 
+            'Bibliography': bibliography, 'Talk': talk_contents}
+    df = pd.DataFrame(data)    
+    return df
+
+while True:
+    print(url)  # here for now to make sure things work. WHEN READY TO TURN IN DELETE
+    if talk_num == 20:  # test the first 20 pages to make sure things work
+        gc_df = make_dataset()  
+
+        sys.exit()
+
+    
+    talk_num += 1
+    url = 'https://scriptures.byu.edu/#:t' + str(talk_num)
+    driver = webdriver.Chrome(path_pc)
+    driver.get(url)
+    get_talk()
 
 # if __name__ == '__main__':
