9f57949256ce2be7c9d7bda840810c03125a6e31
ramsey-king <75282693+ramsey-king@users.noreply.github.com>
2021-07-30 09:25:45 -0500

    30 July 21 0925

---
+++ Wk7-8/KingRamseyWk7-8Twitter.py
@@ -0,0 +1,9 @@
+import twitter
+api = twitter.Api(consumer_key='D8KM23T3iFpwLO8OgQihpSMAX',
+    consumer_secret='HEkmbBUDVtZLTuWq2guji0IRnwQmjHLCI2IJtgH7ZSdT3ju6LW',
+    access_token_key='1419712533322153985-QjB21Rd8UThAzMFHngjRp7iYaoHZet',
+    access_token_secret='XN99oI69OKRgz7Dy4PkR0uc5Di9VxbcOq8hsceh98SJ5S')
+
+print(api.VerifyCredentials())
+
+'https://www.sitepoint.com/how-to-create-a-twitter-app-and-api-interface-via-python/'
